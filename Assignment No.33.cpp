9a620280f4106a3a3394eeab22fb275c297b392f
atharvanaphade <naphade21@gmail.com>
2020-09-26 14:47:25 +0530

    added assign 33

---
+++ Assignment No.33.cpp
@@ -0,0 +1,34 @@
+/* Write a Program to declare a structure of employee which has name , age, salary, mob.no, create a function to display the data */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Employees{
+   char name[100];
+   int age;
+   long long mobilenumber;
+   int salary;
+};
+
+typedef struct Employees emp_data;
+
+void getdata(emp_data emp){
+    printf("Name : ");
+    puts(emp.name);
+    printf("\nAge : %d", emp.age);
+    printf("\nMobile Number : %lli", emp.mobilenumber);
+    printf("\nSalary : %d\n", emp.salary);
+}
+
+int main(){
+    emp_data employees[5];
+    for(int i = 0; i < 5; ++i){
+        printf("\nEnter Name of Employee : ");
+        gets(employees[i].name);
+        printf("\nEnter Age of Employee : ");
+        scanf("%d", &employees[i].age);
+        printf("\nEnter the Mobile Number : ");
+        scanf("%lli", &employees[i].mobilenumber);
+        printf("\nEnter Salary of Employee : ");
+        scanf("%d", &employees[i].salary);
+    }
+}
